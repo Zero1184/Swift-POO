457a8366dedce8619f5d5858bf14953eb742a662
PEDRO BRITO <pedrohenriquebbrito0810@gmail.com>
2025-12-03 18:58:19 +0000

    Adicionando novas classes

--- Sources/Projeto/main.swift
+++ Sources/Projeto/main.swift
@@ -9,14 +9,16 @@ enum NivelAluno{
 
 // Criação classe base
 class Pessoa{
-    var nome: String
-    var email: String
+    private(set) var nome: String
+    private(set) var email: String
    
+    // Construtor da classe base
     init(nome: String, email: String){
         self.nome = nome
         self.email = email
     }
    
+    // Função retorna nome e email
     func getDescricao() -> String{
         return """
         Informações Pessoais:
@@ -26,12 +28,14 @@ class Pessoa{
     }
 }
 
-// Criação da classe Aluno
+// Criação da subclasse Aluno
 class Aluno: Pessoa{
     let matricula: String
     var nivel: NivelAluno
     private(set) var plano: Plano
     
+
+    // Contrutor da subclasse
     init(nome: String, email: String, matricula: String, plano: Plano){
         self.matricula = matricula
         self.nivel = .iniciante
@@ -40,15 +44,17 @@ class Aluno: Pessoa{
     }
 }
 
-// Criação da classe Instrutor
+// Criação da subclasse Instrutor
 class Instrutor: Pessoa{
     let especialidade: String
     
+    // Construtor da subclasse
     init(nome: String, email: String, especialidade: String){
         self.especialidade = especialidade
         super.init(nome: nome, email: email)
     }
     
+    // Sobreescrita da função retorn nome, emial e especialidade
     override func getDescricao() -> String{
         return """
         Nome: \(nome)
@@ -62,21 +68,24 @@ class Instrutor: Pessoa{
 class Plano{
     var nome: String
 
+    // Construtor da classe
     init(nome: String){
         self.nome = nome
     }
 
+    // Função retorna valor da mensalidade
     func calcularMensalidade() -> Double{
         return 0.0
     }
 }
 
-// Plano mensal
+// Criação da subclasse PlanoMensal
 class PlanoMensal: Plano{
     init(){
         super.init(nome: "Plano Mensal")
     }
 
+    // Soobercrita da função retorna valor da mensalidade do plano mensal
     override func calcularMensalidade() -> Double {
         return 120.0
     }
@@ -84,28 +93,21 @@ class PlanoMensal: Plano{
 
 // Plano anual
 class PlanoAnual: Plano{
+
+    // Contrutor da subclasse
     init(){
         super.init (nome: "Plano Anual(Promocional)")
     }
+
+    // Sobreescrita de função retorna mensalidade do plano anual
     override func calcularMensalidade() -> Double{
         let mensalidade: Double = (120 * 0.8)
         return mensalidade
     }
 }
 
-let plano: Plano = Plano(nome:"sla")
+let plano: Plano = PlanoAnual()
 let aluno: Aluno = Aluno(nome:"Pedro", email:"pedro@gmail.com", matricula:"SM23", plano:plano)
 
-let descricao: String = aluno.getDescricao()
-print(descricao)
-print()
-let inst: Instrutor = Instrutor(nome:"Diogo", email:"diogo@gmail.com", especialidade:"Musculacao")
-let desc: String = inst.getDescricao()
-print(desc)
-print()
-let planoM: PlanoMensal = PlanoMensal()
-let m = planoM.calcularMensalidade()
-print(m)
-
-
+print(aluno.plano)
 
--- Sources/Projeto_2/main.swift
+++ Sources/Projeto_2/main.swift
@@ -3,3 +3,16 @@ protocol Manutencao {
     var dataUltimaManutencao: String {get set}
     func realizarManutencao() -> Bool
 }
+
+class Aparelho: Manutencao{
+    var nomeItem: String
+    var dataUltimaManutencao: String
+    
+    init(nome: String){
+        self.nome = nome
+    }
+    
+    realizarManutencao(){
+        print("A última atualização foi feita em \(dataUltimaManutencao)")
+    }
+}
